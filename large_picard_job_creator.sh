6915238f883e0390b645e3b19019262631fbd47b
*joshuaTWalker* <*jtwalker@iastate.edu*>
2017-08-30 10:54:52 -0500

    fixed mistake in 'large_picard_job_creator.sh'

--- large_picard_job_creator.sh
+++ large_picard_job_creator.sh
@@ -3,7 +3,7 @@ module load picard
 ls *.bam > temp.sh
 sed -i -e 's/^/I\=/' temp.sh
 sed -i '1ipicard MergeSamFiles' temp.sh
-echo "O=output_all_bams.bam" >> temp.sh to run
+echo "O=output_all_bams.bam" >> temp.sh
 sed -i -e ':a;N;$!ba;s/\n/ /g' temp.sh
 bash temp.sh
 rm temp.sh temp.she
